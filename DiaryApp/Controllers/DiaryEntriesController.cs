4f6b80659c6abc60d7cc510c69ee2b0f5de64c0f
berkm <berkmahim620@gmail.com>
2024-12-15 23:29:30 +0300

    CRUD Operations for Diaries

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -20,4 +20,74 @@ public class DiaryEntriesController : Controller
         List<DiaryEntry> entries = _context.DiaryEntries.ToList();
         return View(entries);
     }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Create(DiaryEntry entry)
+    {
+        if (entry != null && entry.Title.Length < 3)
+        {
+            ModelState.AddModelError("Title", "Title must be at least 3 characters long");
+        }
+
+        if (ModelState.IsValid)
+        {
+            DiaryEntry newEntry = new DiaryEntry();
+            newEntry.Title = entry.Title.Trim();
+            newEntry.Content = entry.Content.Trim();
+            newEntry.Created = entry.Created;
+        
+            _context.DiaryEntries.Add(newEntry);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(entry);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+        Console.WriteLine(id);
+        DiaryEntry entryToDelete = _context.DiaryEntries.Find(id);
+        _context.DiaryEntries.Remove(entryToDelete);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        DiaryEntry? entry = _context.DiaryEntries.Find(id);
+        if (entry == null)
+        {
+            return NotFound();
+        }
+        return View(entry);
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(DiaryEntry entry)
+    {
+        if (entry != null && entry.Title.Length < 3)
+        {
+            ModelState.AddModelError("Title", "Title must be at least 3 characters long");
+        }
+
+        if (entry.Title.Trim().Length>3)
+        {
+
+            _context.DiaryEntries.Update(entry);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(entry);
+    }
 }
--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -5,10 +5,10 @@ namespace DiaryApp.Models;
 public class DiaryEntry
 {
     public int Id { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Please enter the title of the diary")]
     public string Title { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Please enter the content of the diary")]
     public string Content { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Please enter the Date of the diary")]
     public DateTime Created { get; set; } = DateTime.Now;
 }
